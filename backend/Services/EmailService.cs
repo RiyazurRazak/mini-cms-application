2f71d6fa57b1db5e152226ce7df98d0b1cca19df
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-05-04 14:04:04 +0530

    email service usernmae string literal bug fixed

--- backend/Services/EmailService.cs
+++ backend/Services/EmailService.cs
@@ -15,7 +15,7 @@ namespace cms_api.Services
                 var subject = "You are added to the cms";
                 var to = new EmailAddress(email, username);
                 var plainTextContent = $"You are successfully added to the cms website by the admins. You can access your credentials here. Don't share it with others. Thanks.\r\nTeam CMS Application\r\n Username : {username}\r\n Password : {password}\r\n You can access the account by http://localhost:5173/auth";
-                var htmlContent = "<strong>You are successfully added to the cms website by the admins. You can access your credentials here. Don't share it with others.</strong><p>Thanks</p><p>Team CMS Application</p><h5> Username : {username}</h5><h5> Password : {password}</h5><a href='http://localhost:5173/auth'>You can access the account by click here</a>";
+                var htmlContent = $"<strong>You are successfully added to the cms website by the admins. You can access your credentials here. Don't share it with others.</strong><p>Thanks</p><p>Team CMS Application</p><h5> Username : {username}</h5><h5> Password : {password}</h5><a href='http://localhost:5173/auth'>You can access the account by click here</a>";
                 var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
                 var response = await client.SendEmailAsync(msg);
             }catch (Exception ex)
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -37,13 +37,15 @@ const loginHandller = () => {
 </script>
 
 <template>
-  <div class="panel">
-    <h1 class="title">Login To Access Admin Panel</h1>
-    <InputText v-model="username" type="text" placeholder="username" style="width: 30vw" />
-    <br />
-    <InputText v-model="password" type="password" placeholder="password" style="width: 30vw" />
-    <br />
-    <Button raised rounded size="large" @click="loginHandller">Login</Button>
+  <div class="root">
+    <div class="panel">
+      <h1 class="title">Login To Access Admin Panel</h1>
+      <InputText v-model="username" type="text" placeholder="username" style="width: 30vw" />
+      <br />
+      <InputText v-model="password" type="password" placeholder="password" style="width: 30vw" />
+      <br />
+      <Button raised rounded size="large" @click="loginHandller">Login</Button>
+    </div>
   </div>
 </template>
 
